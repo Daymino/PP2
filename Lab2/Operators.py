867687271abf1b59686c07c60b3968c796a9d6d8
baha <makhsotov.baktybek4@gmail.com>
2024-01-27 15:22:42 +0600

    lab2

---
+++ Lab2/Operators.py
@@ -0,0 +1,23 @@
+#1 Exercise 
+print(10 * 5)
+#Multiplication symbol "*", the Output : 50
+
+#2 Exercise
+print(10/2)
+#Division symbol "/", the Output : 5
+
+#3 Exercise 
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+#Membership operator "in" was used
+  
+#4 Exercise 
+if 5 != 10:
+  print("5 and 10 is not equal")
+#Comparison operator "!=" was used
+  
+#5 Exercise 
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
+#Logical operator "or" was used
