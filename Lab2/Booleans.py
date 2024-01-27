798500f47a914594695003c4d2c3cde3598612be
baha <makhsotov.baktybek4@gmail.com>
2024-01-27 14:59:01 +0600

    lab2

--- Lab2/Booleans.py
+++ Lab2/Booleans.py
@@ -1,3 +1,19 @@
 #1 Exercice
 print (10>9)
 #Ouput : "True"
+
+#2 Exercise
+print (10 == 9)
+#Output : "False"
+
+#3 Exercise
+print(10 < 9)
+#Output : "False"
+
+#4 Exercise
+print(bool("abc"))
+#Output : "True"
+
+#5 Exercise 
+print(bool(0))
+#Output : "False"
