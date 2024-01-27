f3ca731df408193342efd06a9cfa466eace5a065
baha <makhsotov.baktybek4@gmail.com>
2024-01-27 14:22:02 +0600

    lab1

---
+++ Lab1/Numbers.py
@@ -0,0 +1,11 @@
+#1 Exercise
+x = 5
+x = float(x)
+
+#2 Exercise
+x=5.5
+x=int(x)
+
+#3 Exercise
+x=5
+x=complex(x)
---
+++ Lab1/Variables.py
@@ -0,0 +1,27 @@
+#1 Exercise
+carname = "Volvo"
+
+#2 Exercise
+x=50
+
+#3 Exercise
+x=5
+y=10
+print(x+y)
+
+#4 Exercise
+x=5
+y=10
+z=x+y
+print(z)
+
+#5 Exercise
+x, y, z = "Orange", "Banana", "Cherry"
+
+#6 Exercise
+x=y=z="Orange"
+
+#7 Exercise
+def myfunc():
+    global x
+    x = "fantastic"
