52dfa83b7ccdb6c539a5eb5853a049a71d90de3a
baha <makhsotov.baktybek4@gmail.com>
2024-02-09 19:04:55 +0600

    lab3

---
+++ Lab3/Functions1.3.py
@@ -0,0 +1,17 @@
+#Exercise 3
+
+def solve(numheads, numlegs):
+    for num_chickens in range(numheads + 1):
+        num_rabbits = numheads - num_chickens
+        if (2 * num_chickens + 4 * num_rabbits) == numlegs:
+            return num_chickens, num_rabbits
+    return None  # No solution found
+
+heads = 35
+legs = 94
+
+result = solve(heads, legs)
+
+rabbits, chickens = result # Unpack a tuple
+
+print("The number of rabbits is",rabbits,"The number of chickens is", chickens)
