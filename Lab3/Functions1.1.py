bafbf38fc74dc683073b81e063703439dce82137
baha <makhsotov.baktybek4@gmail.com>
2024-02-09 17:14:56 +0600

    lab3

--- Lab3/Functions1.1.py
+++ Lab3/Functions1.1.py
@@ -1,6 +1,8 @@
 #Exercise 1
-def grams_to_ounces_converter(grams):
-    ounces = grams / 28.3495231
+
+def grams_to_ounces(grams):
+    ounces = 28.3495231 * grams
     return ounces
+
 grams = float(input())
-print(grams, "grams equal to", grams_to_ounces_converter(grams), "ounces")
+print(grams_to_ounces(grams))
